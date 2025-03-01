95d3b61e199a052b146293c9485e5d8b30e0d091
erick-kolwaii <eho@kolwaii.com>
2025-03-01 08:35:37 -0700

    update

--- imbalance_avs/Execution_Service/src/task.controller.js
+++ imbalance_avs/Execution_Service/src/task.controller.js
@@ -7,20 +7,27 @@ const dalService = require("./dal.service");
 
 const router = Router()
 
+const MAX_DEVIATION = 0.01
 router.post("/execute", async (req, res) => {
     console.log("Executing task");
 
     try {
+        const pair = "USDC/USDT"
         var taskDefinitionId = Number(req.body.taskDefinitionId) || 0;
         console.log(`taskDefinitionId: ${taskDefinitionId}`);
 
-        const result = await oracleService.getPrice("USDC/USDT");
+        const result = await oracleService.getPrice(pair);
         result.price = req.body.fakePrice || result.price;
-        console.log(result)
-        const cid = await dalService.publishJSONToIpfs(result);
+        let depegged = false;
+        const deviation = Math.abs(result.price - 1.0)
+        if(deviation >= MAX_DEVIATION) {
+          depegged = true;
+        }
+        // We add entropy so that way a fresh fetch is published and fresh task id is generated, as ipfs generates id based off content (i think)
+        const cid = await dalService.publishJSONToIpfs({...result, depegged, pair, entropy: Date.now().toString() });
         const data = "hello";
         await dalService.sendTask(cid, data, taskDefinitionId);
-        return res.status(200).send(new CustomResponse({proofOfTask: cid, data: data, taskDefinitionId: taskDefinitionId}, "Task executed successfully"));
+        return res.status(200).send(new CustomResponse({proofOfTask: cid, data: data, taskDefinitionId: taskDefinitionId, depegged}, "Task executed successfully"));
     } catch (error) {
         console.log(error)
         return res.status(500).send(new CustomError("Something went wrong", {}));
--- imbalance_avs/Validation_Service/src/validator.service.js
+++ imbalance_avs/Validation_Service/src/validator.service.js
@@ -4,16 +4,24 @@ const oracleService = require("./oracle.service");
 
 const MAX_DEVIATION = 0.01
 async function validate(proofOfTask) {
-
   try {
+
+      // Retrieve performer submission
       const taskResult = await dalService.getIPfsTask(proofOfTask);
-      var data = await oracleService.getPrice("USDC/USDT");
-      let isApproved = true;
-      const deviation = Math.abs(taskResult.price - 1.0)
+
+      // Retrieve the submission ourselves to validate 
+      const sourceOfTruth = await oracleService.getPrice(taskResult.pair);
+
+      // Check if our source of truth depgged
+      const depegged = false
+      const deviation = Math.abs(sourceOfTruth.price - 1.0)
       if(deviation >= MAX_DEVIATION) {
-        isApproved = false;
+        depegged = true;
       }
-      return isApproved;
+      
+      // Performer logic should be same as ours, so if it truly did depeg, then it will match on our
+      // validators as well
+      return taskResult.depegged == depegged;
     } catch (err) {
       console.error(err?.message);
       return false;
